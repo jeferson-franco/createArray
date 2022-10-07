ebfa962c5ff863fba5b1686518babe2d66618b18
jefersonfranco <jefersonfranco@protonmail.com>
2022-10-07 07:25:21 -0300

    feat: create createArray.js

---
+++ src/createArray.js
@@ -0,0 +1,12 @@
+function solution(size) {
+    return Array(size).fill(1);
+}
+
+module.exports = solution;
+
+// npm init -y
+// npm i jest-cli -g
+// package.json: "scripts": { "test": "jest" }
+// npm test createArray
+
+// alternative solution
